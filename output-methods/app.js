7c8fe594e77bbdc9e25bf1cac2b682f76941e365
aishaksl <aisha.koseoglu@gmail.com>
2025-04-09 15:03:29 +0300

    feat: add examples and explanations for output-methods

---
+++ output-methods/app.js
@@ -0,0 +1,19 @@
+// document.write()
+document.write("Hello world <br/>");
+document.write("Aisha <br/>");
+document.write(33 + "</br>");
+
+// console.log()
+console.log("hello world");
+console.log(55);
+console.log(true);
+console.log({ name: "aisha", surname: "ksl" });
+let a = 5;
+let b = 4;
+console.log("to sum of two numbers: " + (a + b));
+console.log("to sum of two numbers: ", a + b);
+// to clear the console, we can use console.clear()
+console.clear();
+
+// alert()
+alert("Hello world");
