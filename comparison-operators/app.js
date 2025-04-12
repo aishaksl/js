acea392ed254e14bd82883e604741b59785477fb
aishaksl <aisha.koseoglu@gmail.com>
2025-04-12 11:20:24 +0300

    feat: add examples and explanations for comparison operators

---
+++ comparison-operators/app.js
@@ -0,0 +1,16 @@
+// COMPARISON OPERATORS
+
+/*
+    != : Not equal to
+    >  : Greater than
+    <  : Less than
+    >= : Greater than or equal to
+    <= : Less than or equal to
+*/
+
+console.log(5 == "5"); // true
+console.log(5 === "5"); // false
+console.log(5 != "5"); // false
+console.log(5 !== "5"); // true
+console.log(7 > 3); // true
+console.log(4 <= 4); // true
