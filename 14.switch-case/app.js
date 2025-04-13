b2f1c1d30eca8710ef5ea2463ba9953843c1581b
aishaksl <aisha.koseoglu@gmail.com>
2025-04-13 23:25:03 +0300

    docs: explain basic switch-case syntax in JS

---
+++ 14.switch-case/app.js
@@ -0,0 +1,12 @@
+/* 
+switch (expression) {
+    case value1:
+      // Code to run if expression === value1
+      break;
+    case value2:
+      // Code to run if expression === value2
+      break;
+    default:
+      // Code to run if none of the cases match
+  }
+ */
