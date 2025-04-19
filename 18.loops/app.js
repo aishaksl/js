7592a42f645a3f4be51d9696b7736460941b9c7b
aishaksl <aisha.koseoglu@gmail.com>
2025-04-19 11:48:33 +0300

    chore: add comment lines for loops

--- 18.loops/app.js
+++ 18.loops/app.js
@@ -9,9 +9,11 @@
 
 //FOR
 
-for (initialization; condition; increment / decrement) {
+/*
+  for (initialization; condition; increment / decrement) {
   // code to run each time
 }
+*/
 
 for (var i = 0; i < 5; i++) {
   console.log(i);
@@ -25,18 +27,48 @@ for (var i = 0; i < 5; i++) {
   */
 
 //WHILE
+
+/*
+  while (condition) {
+  // code to run as long as the condition is true
+}
+  */
+
 var i = 0;
 while (i < 5) {
   console.log(i);
   i++;
 }
 
+/*
+    It starts with i = 0.
+    The condition i < 5 is checked before each loop.
+    If the condition is true, the code inside the loop runs.
+    It prints the value of i, then increases i by 1 (i++).
+    When i reaches 5, the condition becomes false, so the loop stops.
+*/
+
 // DO - WHILE;
+
+/*
+  do {
+     // code to run
+  } while (condition);
+*/
+
 var i = 0;
 do {
   console.log(i);
   i++;
 } while (i < 5);
+
+/*
+    It starts with i = 0
+    First, it runs console.log(i) and prints 0
+    Then it increases i by 1
+    Then it checks if i < 5
+    If true, it repeats; if false, it stops
+*/
 
 // FOREACH
 let colors = ["red", "green", "blue"];
