03127a6ef21fb43bd0f85c9c4dd90c9574674f44
aishaksl <aisha.koseoglu@gmail.com>
2025-04-20 23:20:40 +0300

    feat: add explanation and example about arrays

---
+++ 33.array/app.js
@@ -0,0 +1,22 @@
+// ARRAY
+
+// Creating an array
+
+let colors = ["red", "green", "blue"];
+
+//Accessing elements: Use the index (starting from 0):
+
+console.log(colors[0]); // "red"
+console.log(colors[1]); // "green"
+console.log(colors[2]); // "blue"
+
+// Array length: You can get how many elements are in the array
+
+console.log(colors.length); // 3
+
+// Empty array: You can also create an empty array and add items later
+
+let fruit = [];
+fruit.push("apple"); // adds "apple" to the array
+fruit.push("banana"); // adds "banana"
+console.log(fruit); // ["apple", "banana"]
